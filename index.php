0c9ecdd9a58d80347f41d162ddea9c8dc74ff53b
PuttipongMax <maxnetoogo@gmail.com>
2024-06-01 20:37:40 +0700

    feat: show-list products

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
  session_start();
  include 'config.php';
+
+ // product all
+ $query = mysqli_query($conn, "SELECT * FROM products");
+ $rows = mysqli_num_rows($query);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -62,7 +67,66 @@
     <hr class="my-4">
    </form>
   </div>
- </div>
+
+  <div class="row">
+    <div class="col-12">
+      <table class="table table-bordered border-info" >
+        <thead>
+          <tr>
+            <th style="width: 100px;">Image</th>
+            <th>Product Name</th>
+            <th style="width: 200px;">Price</th>
+            <th style="width: 200px;">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php if($rows > 0): ?>
+            <?php while($product = mysqli_fetch_assoc($query)): ?>
+              <tr>
+                <td>
+                  <?php if(!empty($product['profile_image'])): ?>
+                    <img 
+                      src="<?php echo $base_url; ?>/upload_image/<?php echo $product['profile_image']; ?>"
+                      width="100" height="100" alt="Product Image" 
+                    />
+                  <?php else: ?>
+                    <i class="fa-solid fa-image" alt="Product Image"
+                      style="font-size: 100px;"></i>
+                  <?php endif; ?>
+                  </td>
+                  <td>
+                    <?php echo $product['product_name']; ?>
+                    <div>
+                      <small class="text-muted">
+                        <?php echo nl2br($product['detail']); ?>
+                      </small>
+                    </div>
+                  </td>
+                  <td>
+                    <?php echo number_format($product['price'], 2); ?>
+                  </td>
+                  <td>
+                    <a role="button" href="<?php echo $base_url ?>/index.php?id=<?php echo $product['id']; ?>" class="btn btn-outline-dark">
+                      <i class="fa-solid fa-pen-to-square me-1"></i>Edit
+                    </a>
+                    <a onclick="return confirm('Are your sure you want to delete');"
+                      role="button" href="<?php echo $base_url ?>/product-delete.php?id=<?php echo $product['id']; ?>" class="btn btn-outline-danger">
+                      <i class="fa-solid fa-trash-can me-1"></i>Delete
+                    </a>
+                  </td>
+              </tr>  
+               <?php endwhile; ?>   
+            <?php else: ?>
+            <tr>
+              <td colspan="4" >
+                <h4 class="text-center text-danger">ไม่มีรายการสินค้า</h4>
+              </td>
+            </tr> 
+          <?php endif; ?>
+        </tbody>    
+      </table>
+    </div>
+  </div>
 
  <script src="<?php echo $base_url; ?>/assets/js/bootstrap.min.js"></script>
 </body>
