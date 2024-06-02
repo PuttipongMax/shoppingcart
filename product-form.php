7957f6eb9d443a72627c3a83ed36065c57a88168
PuttipongMax <maxnetoogo@gmail.com>
2024-06-02 12:02:26 +0700

    feat: Edit products into database with id.

--- product-form.php
+++ product-form.php
@@ -3,7 +3,7 @@
  include 'config.php';
 
  $product_name = trim($_POST['product_name']);
- $price = trim($_POST['price']);
+ $price = $_POST['price'] ?: 0;
  $detail = trim($_POST['detail']);
  $image_name = $_FILES['profile_image']['name'];
 
@@ -11,8 +11,24 @@
  $folder = 'upload_image/';
  $image_location = $folder . $image_name;
  
- $query = mysqli_query($conn, "INSERT INTO products (product_name, price, profile_image, detail) VALUES ('{$product_name}', '{$price}', '{$image_name}', '{$detail}')") 
-  or die ('query failed');
+ if(empty($_POST['id'])){
+  $query = mysqli_query($conn, "INSERT INTO products (product_name, price, profile_image, detail) VALUES ('{$product_name}', '{$price}', '{$image_name}', '{$detail}')") 
+   or die ('query failed');
+ }else{
+  $query_product = mysqli_query($conn, "SELECT * FROM products WHERE id='{$_POST['id']}'");
+  $result = mysqli_fetch_assoc($query_product);
+
+  if(empty($image_name)){
+   $image_name = $result['profile_image'];
+  }else{
+   @unlink($folder . $result['profile_image']);
+  }
+
+  $query = mysqli_query($conn, "UPDATE products SET product_name='{$product_name}', price='{$price}', profile_image='{$image_name}', detail='{$detail}' WHERE id='{$_POST['id']}'") 
+   or die ('query failed');
+ }
+ 
+mysqli_close($conn);
 
  if($query){
   move_uploaded_file($image_tmp, $image_location);
